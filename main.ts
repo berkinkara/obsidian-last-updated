5d3eacc73f6e8439fb063fe4fba4e462d755c980
berkinkara <25124833+berkinkara@users.noreply.github.com>
2025-05-29 17:02:42 +0200

    Refactor plugin structure and update settings for Last Updated Plugin. Changed class names, updated settings interface, and added new configuration options for auto-update, field name, date format, and default behavior. Updated manifest and package details to reflect new plugin identity.

--- main.ts
+++ main.ts
@@ -1,85 +1,31 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface LastUpdatedSettings {
+	autoUpdateEnabled: boolean;
+	fieldName: string;
+	dateFormat: string;
+	enabledByDefault: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: LastUpdatedSettings = {
+	autoUpdateEnabled: false,
+	fieldName: 'last-updated',
+	dateFormat: 'YYYY-MM-DD HH:mm:ss',
+	enabledByDefault: true
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class LastUpdatedPlugin extends Plugin {
+	settings: LastUpdatedSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// Add settings tab
+		this.addSettingTab(new LastUpdatedSettingTab(this.app, this));
 	}
 
 	onunload() {
-
+		// Plugin cleanup
 	}
 
 	async loadSettings() {
@@ -91,43 +37,63 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class LastUpdatedSettingTab extends PluginSettingTab {
+	plugin: LastUpdatedPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: LastUpdatedPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
+		containerEl.createEl('h2', { text: 'Last Updated Plugin Settings' });
+
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName('Enable auto-update')
+			.setDesc('Master switch to enable or disable automatic last-updated field updates across all files')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.autoUpdateEnabled)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.autoUpdateEnabled = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Field name')
+			.setDesc('The YAML field name to update with the last modified time')
+			.addText(text => text
+				.setPlaceholder('last-updated')
+				.setValue(this.plugin.settings.fieldName)
+				.onChange(async (value) => {
+					this.plugin.settings.fieldName = value || 'last-updated';
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Date format')
+			.setDesc('Format for the date timestamp')
+			.addDropdown(dropdown => dropdown
+				.addOption('YYYY-MM-DD', 'YYYY-MM-DD (2024-01-15)')
+				.addOption('YYYY-MM-DD HH:mm', 'YYYY-MM-DD HH:mm (2024-01-15 14:30)')
+				.addOption('YYYY-MM-DD HH:mm:ss', 'YYYY-MM-DD HH:mm:ss (2024-01-15 14:30:25)')
+				.addOption('ISO', 'ISO (2024-01-15T14:30:25.123Z)')
+				.setValue(this.plugin.settings.dateFormat)
+				.onChange(async (value) => {
+					this.plugin.settings.dateFormat = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Enable by default')
+			.setDesc('Automatically update last-modified field for all files. Can be overridden per file with auto-update-last-modified frontmatter field.')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.enabledByDefault)
+				.onChange(async (value) => {
+					this.plugin.settings.enabledByDefault = value;
 					await this.plugin.saveSettings();
 				}));
 	}
